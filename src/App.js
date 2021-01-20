9fa7bb98983acc118247df07f7eca11aedb647f3
kennycha <dudqn136@naver.com>
2021-01-20 20:22:50 +0900

    드래그로 구간 설정 및 타임라인바 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 import _ from 'lodash';
-import { useState } from "react";
+import { useRef, useState } from "react";
 
-const LONG_DUMMY_INDICES = _.toArray(_.range(0, 100));
-const LONG_DUMMY_IMGS = _.map(_.toArray(_.range(0, 100)), (item, index) => `images/image${item % 15 + 1}.jpeg`);
+const LONG_DUMMY_INDICES = _.toArray(_.range(0, 50));
+const LONG_DUMMY_IMGS = _.map(_.toArray(_.range(0, 50)), (item, index) => `images/image${item % 15 + 1}.jpeg`);
 
 const STANDARD_DUMMY_INDICES = _.toArray(_.range(0, 20));
 
@@ -25,6 +25,7 @@ const PreviewCanvas = styled.canvas`
 `
 
 const Container = styled.div`
+  position: relative;
   margin-top: 15px;
   width: 100%;
   height: 400px;
@@ -33,9 +34,23 @@ const Container = styled.div`
   white-space: nowrap;
 `;
 
+const ClickBox = styled.div`
+  display: inline-block;
+  position: absolute;
+  left: ${({ frameIndex }) => `${frameIndex * 2}%`};
+  width: 2%;
+  height: 100%;
+  background-color: ${({ selected }) => selected ? undefined : 'rgba(41, 41, 41, 0.5)'};
+  cursor: ${({ canDrag }) => canDrag ? 'grab' : 'pointer'};
+  z-index: 30;
+  &:hover {
+    background-color: ${({ canDrag }) => canDrag ? 'rgba(255, 255, 255, 0.7)' : undefined};
+  };
+`
+
 const EditorContainer = styled.div`
   display: inline-block;
-  width: 5%;
+  width: 2%;
   height: 100%;
   color: white;
   z-index: 20;
@@ -61,32 +76,129 @@ const Image = styled.img`
   height: 100%;
   position: absolute;
   z-index: ${({ src }) => !_.isUndefined(src) ? 10 : undefined};
-  /* object-fit: contain; */
+`
+
+const TimeLineBarContainer = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  z-index: 40;
+  width: 1%;
+  min-width: 1.5rem;
+  height: calc(100% - 2rem);
+  top: 2rem;
+  left: ${({ currentFrameIndex }) => `${currentFrameIndex * 2}%`};
+  cursor: pointer;
+`
+
+const TimeLineBar = styled.img`
+  height: 100%;
 `
 
 function App() {
-  const [startFrameIndex, setStartFrameIndex] = useState(3);
-  const [endFrameIndex, setEndFrameIndex] = useState(8);
+  const previewCanvasRef = useRef();
+  const [startFrameIndex, setStartFrameIndex] = useState(0);
+  const [endFrameIndex, setEndFrameIndex] = useState(16);
+  const [currentFrameIndex, setCurrentFrameIndex] = useState(4);
+  const [isStartDragging, setIsStartDragging] = useState(false);
+  const [isEndDragging, setIsEndDragging] = useState(false);
+  const [isBarDragging, setIsBarDragging] = useState(false);
+
+  const ClickComponent = ({ frameIndex }) => {
+    const selected = frameIndex >= startFrameIndex && frameIndex < endFrameIndex;
+    const canDrag = frameIndex === startFrameIndex || frameIndex === endFrameIndex - 1;
+  
+    const handleBoxMouseEnter = (event) => {
+      if (frameIndex === startFrameIndex) {
+        setIsStartDragging(true);
+      } else if (frameIndex === endFrameIndex - 1) {
+        setIsEndDragging(true);
+      }
+    };
+
+    const handleBoxDragStart = (event) => {
+      if (frameIndex === startFrameIndex) {
+        event.target.style.opacity = 0;
+        event.dataTransfer.effectAllowed = 'move';
+        event.dataTransfer.setData('text/html', startFrameIndex);
+      } else if (frameIndex === endFrameIndex - 1) {
+        event.target.style.opacity = 0;
+        event.dataTransfer.effectAllowed = 'move';
+        event.dataTransfer.setData('text/html', endFrameIndex);
+      }
+    };
+
+    const handleBoxDragOver = (event) => {
+      event.preventDefault();
+      if (isStartDragging) {
+        if (frameIndex !== startFrameIndex && frameIndex < endFrameIndex - 15) {
+          setStartFrameIndex(frameIndex);
+        }
+      } else if (isEndDragging) {
+        if (frameIndex !== endFrameIndex && frameIndex >= startFrameIndex + 15) {
+          setEndFrameIndex(frameIndex + 1);
+        }
+      }
+    };
+
+    const handleBoxDragEnd = (event) => {
+      if (isStartDragging) {
+        event.target.style.opacity = 1;
+        setIsStartDragging(false);
+      } else if (isEndDragging) {
+        event.target.style.opacity = 1;
+        setIsEndDragging(false);
+      }
+    };
+
+    const handleBoxMouseOut = (event) => {
+      if (isStartDragging) {
+        setIsStartDragging(false);
+      } else if (isEndDragging) {
+        setIsEndDragging(false);
+      }
+    };
+
+    return <ClickBox
+      onMouseEnter={handleBoxMouseEnter}
+      onMouseOut={handleBoxMouseOut}
+      onDragStart={handleBoxDragStart}
+      onDragOver={handleBoxDragOver}
+      onDragEnd={handleBoxDragEnd}
+      frameIndex={frameIndex}
+      selected={selected}
+      canDrag={canDrag}
+      draggable={canDrag}
+      />
+  }
   
   const EditorComponent = ({ frameIndex }) => {
-    const isEmpty = frameIndex >= SHORT_DUMMY_INDICES.length;
+    const isEmpty = frameIndex >= LONG_DUMMY_INDICES.length;
+    
     return (
       <EditorContainer>
-        <IndexContainer selected={frameIndex >= startFrameIndex && frameIndex <= endFrameIndex}>
+        <IndexContainer selected={frameIndex >= startFrameIndex && frameIndex < endFrameIndex}>
           {frameIndex}
         </IndexContainer>
         <ImageContainer empty={isEmpty} >
-          <Image src={frameIndex % 5 === 0 ? SHORT_DUMMY_IMGS[frameIndex] : undefined} />
+          <Image src={frameIndex % 5 === 0 ? LONG_DUMMY_IMGS[frameIndex] : undefined} />
         </ImageContainer>
       </EditorContainer>)
-  }  
+  }
 
   return (
     <>
       <Title>Editor Sample</Title>
-      <PreviewCanvas />
+      <PreviewCanvas ref={previewCanvasRef} />
       <Container>
-        {_.map(_.lt(SHORT_DUMMY_INDICES.length, 30) ? STANDARD_DUMMY_INDICES : SHORT_DUMMY_INDICES, (frameIndex) => {
+        <TimeLineBarContainer 
+          currentFrameIndex={currentFrameIndex} 
+          draggable
+        >
+          <TimeLineBar src='images/timeline_bar.svg' draggable={false} />
+        </TimeLineBarContainer>
+        {_.map(_.lt(LONG_DUMMY_INDICES.length, 30) ? STANDARD_DUMMY_INDICES : LONG_DUMMY_INDICES, (frameIndex) => <ClickComponent frameIndex={frameIndex} />)}
+        {_.map(_.lt(LONG_DUMMY_INDICES.length, 30) ? STANDARD_DUMMY_INDICES : LONG_DUMMY_INDICES, (frameIndex) => {
           return <EditorComponent frameIndex={frameIndex} />
         })}
       </Container>

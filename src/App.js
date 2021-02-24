1e67d77e1da467ed16e3e868db2bd962f429ece7
kennycha <dudqn136@naver.com>
2021-02-24 20:13:19 +0900

    complete refactoring frame-cut by dragging

--- src/App.js
+++ src/App.js
@@ -1,41 +1,156 @@
 import _ from 'lodash';
-import React, { useRef, useState } from "react";
-import { Container, FlexBox, Image, Index, LeftMask, LeftMaskDrag, RightMask, RightMaskDrag } from "./Components";
+import React, { useEffect, useRef, useState } from "react";
+import { Container, EmptyBox, FlexBox, Image, Index, IndexBackground, LeftMask, LeftMaskDrag, RightMask, RightMaskDrag } from "./Components";
 
-const LONG_DUMMY_INDICES = _.toArray(_.range(0, 100));
-const LONG_DUMMY_IMGS = _.map(_.toArray(_.range(0, 100)), (item, index) => `images/image${item % 15 + 1}.jpeg`);
+const LONG_DUMMY_INDICES = _.toArray(_.range(0, 87));
+const LONG_DUMMY_IMGS = _.map(_.toArray(_.range(0, 87)), (item, index) => `images/image${item % 15 + 1}.jpeg`);
 
 const STANDARD_DUMMY_INDICES = _.toArray(_.range(0, 20));
 
 const SHORT_DUMMY_INDICES = _.toArray(_.range(0, 11));
 const SHORT_DUMMY_IMGS = _.map(_.toArray(_.range(0, 11)), (item, index) => `images/image${item % 15 + 1}.jpeg`);
 
-
-
-
 function App() {
   const [startFrameIndex, setStartFrameIndex] = useState(4);
-  const [endFrameIndex, setEndFrameIndex] = useState(16);
+  const [endFrameIndex, setEndFrameIndex] = useState(20);
   const [isStartDragging, setIsStartDragging] = useState(false);
   const [isEndDragging, setIsEndDragging] = useState(false);
-  const [isBarDragging, setIsBarDragging] = useState(false);
+  
+  let innerStart;
+  let innerEnd;
 
+  useEffect(() => {
+    if (_.isUndefined(startFrameIndex) && !_.isUndefined(innerStart)) {
+      setStartFrameIndex(innerStart);
+    }
+    if (_.isUndefined(endFrameIndex) && !_.isUndefined(innerEnd)) {
+      setEndFrameIndex(innerEnd);
+    }
+  }, [endFrameIndex, innerEnd, innerStart, startFrameIndex])
+
+  const handleLeftDragStart = (event) => {
+    if (!isStartDragging) {
+      setIsStartDragging(true);
+    }
+    event.target.style.opacity = 0;
+    event.dataTransfer.effectAllowed = 'move';
+    event.dataTransfer.setData('text/html', startFrameIndex);
+  }
+
+  
+  const handleRightMouseEnter = (event) => {
+    if (!isEndDragging) {
+      setIsEndDragging(true);
+    }
+  }
+  
+  const handleRightDragStart= (event) => {
+    event.target.style.opacity = 0;
+    event.dataTransfer.effectAllowed = 'move';
+    event.dataTransfer.setData('text/html', endFrameIndex);
+  }
+  
+  const handleDragOver = (event, frameIndex) => {
+    event.preventDefault();
+    if (isStartDragging && frameIndex !== startFrameIndex && frameIndex < endFrameIndex - 15) {
+      innerStart = frameIndex;
+    } else if (isEndDragging && frameIndex !== endFrameIndex && frameIndex >= startFrameIndex + 15) {
+      innerEnd = frameIndex + 1;
+    }
+  }
+  
+  const handleDragEnd = (event) => {
+    if (isStartDragging) {
+      event.target.style.opacity = 1;
+      setIsStartDragging(false);
+      if (!_.isUndefined(innerStart)) {
+        setStartFrameIndex(innerStart);
+        innerStart = 0;
+      } else {
+        innerStart = startFrameIndex;
+      }
+    } else if (isEndDragging) {
+      event.target.style.opacity = 1;
+      setIsEndDragging(false);
+      if (!_.isUndefined(innerEnd)) {
+        setEndFrameIndex(innerEnd);
+        innerEnd = 0;
+      } else {
+        innerEnd = endFrameIndex;
+      }
+    }
+  }
+
+  const handleMouseOut = (event) => {
+    if (isStartDragging) {
+      event.target.style.opacity = 1;
+      setIsStartDragging(false);
+    } else if (isEndDragging) {
+      event.target.style.opacity = 1;
+      setIsEndDragging(false);
+    }
+  }
 
   return (
     <Container>
+      <IndexBackground
+        left={`${startFrameIndex * 2}%`}
+        width={`${(endFrameIndex - startFrameIndex) * 2}%`}
+      />
       <LeftMask startFrameIndex={startFrameIndex} />
-      <RightMask endFrameIndex={endFrameIndex} />
+      <RightMask
+        endFrameIndex={endFrameIndex}
+        width={`${((_.lt(LONG_DUMMY_INDICES.length, 50) ? STANDARD_DUMMY_INDICES.length : LONG_DUMMY_INDICES.length) - endFrameIndex) * 2}%`} />
+      {_.map(
+        _.lt(LONG_DUMMY_INDICES.length, 50) ? STANDARD_DUMMY_INDICES : LONG_DUMMY_INDICES,
+        (frameIndex, idx) => (
+          <FlexBox 
+            key={`flex-${idx}`}
+            frameIndex={frameIndex}
+            column
+            width='2%'
+            zIndex={30}
+          >
+            <Index key={`index-${idx}`}>
+              {frameIndex}
+            </Index>
+            <Image
+              key={`image-${idx}`}
+              wide={frameIndex % 5 === 0}
+              draggable={false}
+              src={frameIndex % 5 === 0 ? LONG_DUMMY_IMGS[frameIndex] : undefined}
+            />
+          </FlexBox>)
+      )}
+      <FlexBox
+        width={`${(_.lt(LONG_DUMMY_INDICES.length, 50) ? STANDARD_DUMMY_INDICES.length : LONG_DUMMY_INDICES.length) * 2}%`}
+        zIndex={50}
+      >
         {_.map(
-          _.lt(LONG_DUMMY_INDICES.length, 30) ? STANDARD_DUMMY_INDICES : LONG_DUMMY_INDICES,
-          (frameIndex) => (
-            <FlexBox frameIndex={frameIndex} column>
-              <Index>{frameIndex}</Index>
-              <Image src={frameIndex % 5 === 0 ? LONG_DUMMY_IMGS[frameIndex] : undefined} />
-            </FlexBox>)
-        )}
-      
-      <LeftMaskDrag startFrameIndex={startFrameIndex} />
-      <RightMaskDrag endFrameIndex={endFrameIndex} />
+          _.lt(LONG_DUMMY_INDICES.length, 50) ? STANDARD_DUMMY_INDICES : LONG_DUMMY_INDICES,
+         (frameIndex, idx) => (
+          <EmptyBox
+            onDragOver={(event) => handleDragOver(event, frameIndex)}
+            frameIndex={frameIndex}
+            key={`emptybox-${idx}`}
+          />
+        ))}
+      </FlexBox>
+      <LeftMaskDrag 
+        startFrameIndex={startFrameIndex}
+        draggable
+        onDragStart={handleLeftDragStart}
+        onDragEnd={handleDragEnd}
+        onMouseOut={handleMouseOut}
+      />
+      <RightMaskDrag 
+        endFrameIndex={endFrameIndex}
+        draggable
+        onMouseEnter={handleRightMouseEnter}
+        onDragStart={handleRightDragStart}
+        onDragEnd={handleDragEnd}
+        onMouseOut={handleMouseOut}
+      />
     </Container>
   );
 }
--- src/Components.js
+++ src/Components.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   position: relative;
-  width: 100%;
+  width: ${({ width }) => width};
   height: 400px;
   overflow-x: scroll;
   white-space: nowrap;
@@ -14,56 +14,88 @@ export const LeftMask = styled.div`
   height: 100%;
   display: inline-block;
   position: absolute;
+  z-index: 40;
+  background-color: rgba(0, 0, 0, 0.7);
   top: 0;
   left: 0;
 `
 
 export const LeftMaskDrag = styled.div`
   width: 2%;
-  height: calc(100% - 4rem);
+  height: calc(100% - 40px);
   position: absolute;
+  z-index: 60;
   top: 0;
   left: ${({ startFrameIndex }) => `${startFrameIndex * 2}%`};
-  margin-top: 3rem;
+  margin-top: 30px;
+  cursor: pointer;
+
   background-color: yellow;
 `
 
 export const RightMask = styled.div`
-  width: ${({ endFrameIndex }) => `${100 - (endFrameIndex * 2)}%`};
+  width: ${({ width }) => width};
   height: 100%;
   display: inline-block;
   position: absolute;
+  z-index: 40;
+  background-color: rgba(0, 0, 0, 0.7);
   top: 0;
-  right: 0;
+  left: ${({ endFrameIndex }) => `${(endFrameIndex) * 2}%`};
 `
 
 export const RightMaskDrag = styled.div`
   width: 2%;
-  height: calc(100% - 4rem);
+  height: calc(100% - 40px);
   position: absolute;
+  z-index: 60;
   top: 0;
-  left: ${({ endFrameIndex }) => `${endFrameIndex * 2}%`};
-  margin-top: 3rem;
+  left: ${({ endFrameIndex }) => `${(endFrameIndex - 1) * 2}%`};
+  margin-top: 30px;
+  cursor: pointer;
+
   background-color: yellow;
 `
 
 export const Index = styled.div`
   width: 100%;
-  height: 2rem;
+  height: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: rgb(189, 189, 189);
+`
+
+export const IndexBackground = styled.div`
+  width: ${({ width }) => width};
+  height: 20px;
+  position: absolute;
+  border-radius: 5px;
+  left: ${({ left }) => left};
+  background-color: #3785F7;
 `
 
 export const Image = styled.img`
-  width: 100%;
-  margin-top: 1rem;
-  margin-bottom: 1rem; 
-  height: calc(100% - 4rem);
+  width: ${({ wide }) => wide ? '500%' : '0'};
+  z-index: 10;
+  margin-top: 10px;
+  margin-bottom: 10px; 
+  height: calc(100% - 40px);
 `
 
 export const FlexBox = styled.div`
-width: 2%;
+  width: ${({ width }) => width};
   height: 100%;
   display: flex;
   flex-direction: ${({ column }) => column ? 'column' : undefined};
   position: absolute;
+  z-index: ${({ zIndex }) => zIndex};
   left: ${({ frameIndex }) => `${frameIndex * 2}%`};
+`
+
+export const EmptyBox = styled.div`
+  width: 2%;
+  height: 100%;
+  left: ${({ frameIndex }) => `${frameIndex * 2}%`};
+  opacity: 0;
 `
